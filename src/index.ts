fa526a11281af3431a31f07996cbba5d32e034fc
dimmdimas <mdrdimas1@gmail.com>
2025-01-20 01:38:23 +0700

    fix vercel

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,22 @@ import router from "./routes/api";
 import bodyParser from "body-parser";
 import db from "./untils/database"
 
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running' , data: null});
+});
+
+app.use('/api', router);
+
 async function init() {
     try {
-        dotenv.config();
-        const PORT = process.env.PORT;
-        const app = express();
-        app.use(bodyParser.json());
-
         // connect to database
         const dbStatus = await db();
         console.log(dbStatus);
-
-        app.get('/', (req, res) => {
-            res.status(200).json({ message: 'Server is running' , data: null});
-        });
-
-        // middleware
-        app.use('/api', router);
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
@@ -34,4 +33,4 @@ async function init() {
 
 init();
 
-export default init;    
+export default app;    
